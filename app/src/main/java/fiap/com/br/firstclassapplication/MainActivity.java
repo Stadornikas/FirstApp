81a50270586efad2e956cee95e242223399754f4
logonrm <logonrm@fiap.com.br>
2017-02-14 22:35:36 -0200

    initial commit

---
+++ app/src/main/java/fiap/com/br/firstclassapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package fiap.com.br.firstclassapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mostrarMenssagem(View v){
+        Toast.makeText(this, R.string.message, Toast.LENGTH_LONG).show();
+    }
+}
